7d18e1252d179c8d76c5ed63f83fac60cd1c17d9
carlosmn1997 <carlosmn1997@gmail.com>
2018-03-12 13:02:30 +0100

    cambios en la consulta

--- p1p2/entrega/fuentes/consulta.sql
+++ p1p2/entrega/fuentes/consulta.sql
@@ -4,15 +4,25 @@ metálico
 */
 
 CREATE VIEW primeraCuenta AS
-SELECT IBAN FROM Cuenta
-WHERE numero = (SELECT MIN(numero) FROM Cuenta);
+SELECT IBANOrigen FROM Operacion
+WHERE fecha = (SELECT MIN(FECHA) FROM Operacion);
 
 CREATE VIEW Clienteprim AS
-SELECT Nombre, Apellidos FROM Cliente
-WHERE DNI IN (SELECT DNI FROM Posee WHERE IBAN IN (SELECT IBAN FROM primeraCuenta));
+SELECT Nombre, Apellido FROM Cliente
+WHERE DNI IN (SELECT DNI FROM Posee WHERE IBAN IN (SELECT IBANORIGEN FROM primeraCuenta));
 
-SELECT C.Nombre, C.Apellidos, SUM(O.cantidad) AS Ingresos FROM Clienteprim C, Operaciones O
-WHERE O.IBANOrigen IN (SELECT IBAN FROM primeraCuenta);
+CREATE VIEW Ingre AS
+SELECT SUM(cantidad) AS Ingresos FROM Operacion
+WHERE IBANOrigen IN (SELECT IBANOrigen FROM primeraCuenta);
+
+SELECT C.Nombre, C.Apellido, I.Ingresos FROM Clienteprim C, Ingre I;
 
 DROP VIEW primeraCuenta;
 DROP VIEW Clienteprim;
+DROP VIEW Ingre;
+
+
+SELECT cantidad,tipo FROM OPERACION WHERE IBANorigen = 'TR80 8894 8SRY 3VWO XU1H Q6FS 95';
+
+SELECT * FROM Posee 
+WHERE DNI IN (SELECT DNI FROM CLIENTE) AND IBAN IN (SELECT IBAN FROM Cuenta);
