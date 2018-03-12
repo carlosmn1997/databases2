ce6956883cc23aba20df3573fec1e41265f835d9
nico_lera17 <nleralopez@gmail.com>
2018-03-12 12:19:52 +0100

    consultas 1 y 2

---
+++ p1p2/entrega/fuentes/consulta.sql
@@ -0,0 +1,18 @@
+/*
+Cliente que creo la primera cuenta y cuanto ha obtenido en ingresos en
+metálico
+*/
+
+CREATE VIEW primeraCuenta AS
+SELECT IBAN FROM Cuenta
+WHERE numero = (SELECT Min(numero) FROM Cuenta);
+
+CREATE VIEW Clienteprim AS
+SELECT Nombre, Apellidos FROM Cliente
+WHERE DNI IN (SELECT DNI FROM Posee WHERE IBAN IN (SELECT IBAN FROM primeraCuenta));
+
+SELECT C.Nombre, C.Apellidos, SUM(O.cantidad) FROM Clienteprim C, Operaciones O
+WHERE O.IBANOrigen IN (SELECT IBAN FROM primeraCuenta);
+
+DROP VIEW primeraCuenta;
+DROP VIEW Clienteprim;
---
+++ p1p2/entrega/fuentes/consulta2.sql
@@ -0,0 +1,19 @@
+/*
+Oficina de las 10 que menos cuentas tienen que registra más retiradas
+*/
+
+CREATE VIEW topOficinas AS
+SELECT Oficina, COUNT(IBAN) AS numCuentas FROM Cuenta
+WHERE tipo = 'corriente'
+GROUP BY Oficina
+ORDER BY numCuentas DESC
+WHERE ROWNUM<=10;
+
+CREATE VIEW topRetiradas AS
+SELECT Oficina, COUNT(codigo) AS numRetiradas FROM Oficina
+WHERE tipo = 'retirada'
+GROUP BY Oficina;
+
+SELECT codigo, direccion FROM Oficina
+WHERE (codigo IN (SELECT codigo FROM topOficinas)) AND
+(codigo IN (SELECT codigo FROM topRetiradas WHERE numRetiradas = (SELECT Max(numRetiradas) FROM topRetiradas)))
