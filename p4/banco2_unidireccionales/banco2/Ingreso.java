b2aeab74d68ae8fae00709d62b007206f4a4654f
carlosmn1997 <carlosmn1997@gmail.com>
2018-05-09 10:03:02 +0200

    parte 2 con relaciones unidireccionales

---
+++ p4/banco2_unidireccionales/banco2/Ingreso.java
@@ -0,0 +1,35 @@
+package banco2;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+@DiscriminatorValue("ingreso")
+public class Ingreso extends Operacion{
+  private Oficina oficinaByOficina;
+  @ManyToOne
+  @JoinColumn(name = "OFICINA", referencedColumnName = "CODIGO", insertable = false, updatable = false)
+  public Oficina getOficinaByOficina() {
+      return oficinaByOficina;
+  }
+
+  public void setOficinaByOficina(Oficina oficinaByOficina) {
+      this.oficinaByOficina = oficinaByOficina;
+  }
+
+}
